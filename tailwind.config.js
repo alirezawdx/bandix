cdfdbfd5714ccbb2a55709ead0c748c7df78dda6
Alireza Sadeghi <alirezawdx@gmail.com>
2024-07-18 18:17:42 +0330

    feat: add preferred colors to tailwind conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,14 @@ const colors = require('tailwindcss/colors');
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
 	theme: {
-		colors: { ...colors },
+		colors: {
+			...colors,
+			primary: '#FF8200',
+			secondary: '#FFC72E',
+			dark: '#0A0903',
+		},
 		fontFamily: {
-			dana: ['Dana', 'sans-serif',],
+			dana: ['Dana', 'sans-serif'],
 		},
 		extend: {},
 	},
